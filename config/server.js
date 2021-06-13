620799a9c2a98ea2d21096107c603a2b07cb97de
seta-thanhdat <datnt3011@seta-international.vn>
2021-06-13 14:25:28 +0700

    fix/url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("WEBSITE", "http://127.0.0.1:1337"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "602e37eec222d64dd1f8498ab857942a"),
